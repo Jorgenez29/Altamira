5e24c3ac3dcaaec50d136e85ac3caaa3ea247fa4
Jorgenez29 <87529565+Jorgenez29@users.noreply.github.com>
2023-06-13 22:36:38 +0200

    Add data.json. Display data json in HTML

--- ng/src/app/app.component.ts
+++ ng/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import studentsData from '../assets/data.json';
+  
+interface Student {
 
+  nombre: String;
+  libros: Array<string>;
+
+}
+  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'ng';
+export class AppComponent  {
+  name = 'Angular';
+  students: Student[] = studentsData;
+
 }
